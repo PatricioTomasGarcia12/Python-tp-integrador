dc2fff9fc20d44e24b6998f86e9348f780e223c6
PatricioTomasGarcia12 <patricio.garcia.tomas@gmail.com>
2026-04-15 12:05:27 -0300

    Agrega la funcionalidad del ejercicio 3.B

--- src/ej03_funciones.py
+++ src/ej03_funciones.py
@@ -26,3 +26,13 @@ def coordenadas_incorrectas(ruta, encoding, separador, nombreLatitud, nombreLong
         csv_reader = csv.DictReader(archivo, delimiter=separador)
         registros = [row for row in csv_reader if not (validar_latitud(row, nombreLatitud) and validar_longitud(row, nombreLongitud))]
         return len(registros), registros 
+
+
+def solo_existe_longitud(linea, nombreLatitud, nombreLongitud):
+    return campo_vacio(linea, nombreLatitud) and not campo_vacio(linea, nombreLongitud) 
+
+def solo_existe_latitud(linea, nombreLatitud, nombreLongitud):
+    return campo_vacio(linea, nombreLongitud) and not campo_vacio(linea, nombreLatitud)
+
+def solo_latitud_o_longitud(linea, nombreLatitud, nombreLongitud):
+    return solo_existe_latitud(linea, nombreLatitud, nombreLongitud) ^ solo_existe_longitud(linea, nombreLatitud, nombreLongitud)
