515f94642358e26b18caa714e765cbc7251b42b4
felipebertora <bertorafelipe11@gmail.com>
2026-04-15 23:28:18 -0300

    agrego 2.I y 2.J

--- src/ej02_funciones.py
+++ src/ej02_funciones.py
@@ -78,3 +78,83 @@ def frecuencia_columna(ruta, nombre_columna, delimiter = ","):
         csv_reader = csv.DictReader(f_csv, delimiter = delimiter)
         #Dado el nombre de una columna, retornar frecuencia de cada valor distinto:
         return Counter(row[nombre_columna] for row in csv_reader)
+     
+def analizar_columna(ruta, nombre_columna, tipo, delimiter=","):
+    with open(ruta, 'r', encoding="utf-8") as f_csv:
+        reader = csv.DictReader(f_csv, delimiter=delimiter)
+        
+        # Verifica si la columna existe
+        if nombre_columna not in reader.fieldnames:
+            print("La columna no existe")
+            return
+        
+        valores = []
+        
+        for row in reader:
+            valor = row[nombre_columna]
+            
+            # ignorar vacíos
+            if valor is None or valor.strip() == "":
+                continue
+            # agrega a la lista
+            valores.append(valor)
+        
+        if len(valores) == 0:
+            print("No hay valores válidos en esa columna")
+            return
+        
+        #si es tipo numerico
+        if tipo == "numeric":
+         nums = []
+    
+         for v in valores:
+             try:
+                num = float(v)
+                nums.append(num)
+             except:
+               continue
+         
+         if len(nums) == 0:
+             print("No hay valores numéricos válidos en la columna")
+             return
+    
+         return min(nums), max(nums), sum(nums)/len(nums)
+        
+
+
+        #si es tipo cordinate
+        elif tipo == "coordinate":
+            nums = [float(v) for v in valores]
+            return min(nums), max(nums)
+        
+        #si es tipo texto
+        elif tipo == "text":
+            largos = [len(v) for v in valores]
+            return min(largos), max(largos)
+        
+        else:
+            print("tipo no válido")
+
+
+
+
+def columnas_totalmente_vacias(ruta, delimiter=","):
+    with open(ruta, 'r', encoding="utf-8") as f_csv:
+        reader = csv.DictReader(f_csv, delimiter=delimiter)
+        columnas = reader.fieldnames
+        
+        #primeroasume que son todas vacias
+        vacias = {col: True for col in columnas}
+        for row in reader:
+            for col in columnas:
+                valor = row[col]
+
+                if valor is not None and valor.strip() != "":
+                    #saca las que no son vacias
+                    vacias[col] = False
+        
+        resultado = []
+        for col in vacias:
+            if vacias[col]:
+                resultado.append(col) 
+        return resultado
